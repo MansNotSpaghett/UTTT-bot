984aba044f927cc7ce91e477b069a5d2323b120c
MansNotSpaghett <okyanuscan_acar@yahoo.com>
2018-07-30 10:35:08 +0300

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -202,6 +202,7 @@ so selecting "1" would select the top left board. the same thing applies for the
                         setattr(board,"b"+str(x)+str(y),"   ")
                 #setattr(boardList,"bLU",[[0,0,0],[0,0,0],[0,0,0]])
                 break
+            await ext.channel.send("here too")
             if rules.theWinner(getattr(boardList, "bL"+str(put)[0])) != 0:
                 for x in range(1, 10):
                     setattr(board,"b"+str(put)[0]+str(x), X[-x] if rules.theWinner(getattr(boardList, "bL"+str(put)[0])) == 1 else O[-x])
